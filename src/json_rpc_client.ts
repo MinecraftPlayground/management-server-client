0f187afdb6aaf47d82ae43718d7da5ec18d0d141
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-02-04 19:56:36 +0100

    moved

--- src/json_rpc_client.ts
+++ src/json_rpc_client.ts
@@ -1,6 +1,6 @@
 import type { ExtractMethod } from './open_rpc/method/extract_method.ts';
 import type { ExtractMethodNames } from './open_rpc/method/extract_method_names.ts';
-import type { ExtractParams } from './open_rpc/params/extract_params.ts';
+import type { ExtractParams } from './open_rpc/method/params/extract_params.ts';
 import type { ExtractResult } from './open_rpc/result/extract_result.ts';
 import type { MethodOrReference, OpenRpcDocument } from './open_rpc/open_rpc_document.ts';
 import { validatedOpenRpcDocument, type ValidatedOpenRpcDocument } from './open_rpc/validated_open_rpc_document.ts';
