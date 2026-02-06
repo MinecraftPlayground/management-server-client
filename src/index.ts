9e1df456cc41362ca2a3accb19d7007ac9090e29
Mqxx <62719703+Mqxx@users.noreply.github.com>
2026-02-06 09:56:30 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import schema from '../example_schemas/json_rpc_api_schema.ts';
-import { JsonRpcClient } from './json_rpc_client.ts';
+import { Client } from './client.ts';
 import { validatedOpenRpcDocument } from './open_rpc/validated_open_rpc_document.ts';
 
 
 validatedOpenRpcDocument(schema)
 
-const client = new JsonRpcClient('', schema)
+const client = new Client('', schema)
  
 client.call('minecraft:allowlist')
 
 client.addEventListener('minecraft:notification/allowlist/added', (event) => {
-  event
+  event.detail.forEach((player) => {
+    player
+  })
 })
